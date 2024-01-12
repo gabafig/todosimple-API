f564099c28519ff0d0ec87ea6bfbedf854e05f1e
GabrielCIF <gabriel.cazzeri@gmail.com>
2024-01-12 08:23:34 -0300

    Create UserSpringSecurity

---
+++ src/main/java/com/gabrielcazzeri/todosimple/security/UserSpringSecurity.java
@@ -0,0 +1,65 @@
+package com.gabrielcazzeri.todosimple.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.gabrielcazzeri.todosimple.models.enums.ProfileEnum;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+    public UserSpringSecurity(Long id, String username, String password, Set<ProfileEnum> ProfileEnums) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = ProfileEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasRole(ProfileEnum profileEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+
+}
